412a7f05ec57e10bc77095cc1c8b60141f49ef71
NimbusStudios <nimbusstudios917@gmail.com>
2024-07-18 12:04:00 +0200

    Updated

--- employee-tracker/tailwind.config.js
+++ employee-tracker/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
+  theme: { 
     extend: {
       colors: {
         'primary': '#66EB9A',  // Green from screenshots
@@ -23,6 +23,26 @@ module.exports = {
       // You can add more custom styles here, for example:
       boxShadow: {
         'card': '0px 4px 16px rgba(0, 0, 0, 0.1)',
+      },
+      screens: {
+        '3xl': '1600px',
+        '4xl': '1920px',
+        '5xl': '2560px',
+        '6xl': '3840px',
+        '7xl': '5120px',
+        '8xl': '6400px',
+        '9xl': '7680px',
+        '10xl': '8960px',
+        '11xl': '10240px',
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 0.5s ease-in-out',
       },
     },
   },
